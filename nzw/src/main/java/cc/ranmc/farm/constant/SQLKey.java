877de69e678a6b722f6d2e712ebaf3e15ecfc8de
minglaoban666 <158486293+minglaoban666@users.noreply.github.com>
2025-07-15 02:20:10 +0800

    Delete nzw directory

--- nzw/src/main/java/cc/ranmc/farm/constant/SQLKey.java
+++
@@ -1,8 +0,0 @@
-package cc.ranmc.farm.constant;
-
-public class SQLKey {
-
-    public static final String ID = "ID";
-    public static final String PLAYER = "PLAYER";
-    public static final String OPEN = "OPEN";
-}
